13fd46562efab8f517f5a4e439aa4c816676e871
luolei <luo.lei@thebizark.com>
2023-02-27 17:05:51 +0800

    fix: 提交测试

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,63 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
-import Nav from './components/Nav.vue';
-import Footer from './components/Footer.vue';
+import { RouterView } from "vue-router";
+import Nav from "./components/Nav.vue";
+import Footer from "./components/Footer.vue";
+import Utils from "./utils/Utils";
+import { ref, type Ref } from "vue";
+let showScroller: Ref<boolean> = ref(false);
+(document.querySelector('body') as HTMLBodyElement).onscroll = () => {
+    if (window.scrollY >= 500) {
+        showScroller.value = true;
+    }
+    else {
+        showScroller.value = false;
+    }
+};
+function scrollTop() {
+    Utils.scrollIntoElement("nav");
+}
 </script>
 
 <template>
     <div class="container">
-        <Nav></Nav>
+        <Nav id="nav"></Nav>
         <div class="view-container">
             <RouterView></RouterView>
         </div>
         <Footer></Footer>
+        <Transition name="fade">
+            <div v-if="showScroller" class="scroll-top" @click="scrollTop">
+                <i class="fa-solid fa-circle-arrow-up"></i>
+            </div>
+        </Transition>
     </div>
 </template>
 
 <style lang="less" scoped>
-@import './assets/common.less';
+@import "./assets/common.less";
 .container {
+    position: relative;
     .view-container {
         background-color: #6e6e73;
+    }
+    .scroll-top {
+        position: fixed;
+        bottom: 50px;
+        right: 50px;
+        padding: 10px;
+        color: #fff;
+        font-size: 26px;
+        cursor: pointer;
+    }
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: all 100ms ease;
+    }
+    .fade-enter-from,
+    .fade-leave-to {
+        // position: absolute;
+        opacity: 0;
+        transform: translateY(-10px);
     }
 }
 </style>
--- src/views/TechnologyStack.vue
+++ src/views/TechnologyStack.vue
@@ -1,17 +1,29 @@
 <template>
     <div class="technology-stack">
-        <div class="tech-title">{{tecData.name}}</div>
+        <div class="tech-title">{{ tecData.name }}</div>
         <div class="tech-item" v-for="item in tecList">
             <div class="tech-item-title" :id="item.name">{{ item.name }}</div>
+            <div class="tech-item-remark">自我评价：{{ getText(item.score) }}</div>
             <div class="tech-item-description">{{ item.description }}</div>
         </div>
     </div>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const props = defineProps(["tecData"]);
 let tecList = ref(props.tecData.data);
+function getText(score: number): string {
+    let text = "--";
+    if (score <= 50) {
+        text = "了解";
+    } else if (score > 50 && score <= 80) {
+        text = "比较熟悉";
+    } else if (score > 80) {
+        text = "熟悉";
+    }
+    return text;
+}
 </script>
 <style lang="less" scoped>
 .technology-stack {
@@ -27,6 +39,10 @@ let tecList = ref(props.tecData.data);
             font-size: 50px;
             font-weight: bold;
             padding-bottom: 20px;
+        }
+        .tech-item-remark {
+            font-size: @commonFontSize;
+            margin: 15px 0;
         }
         .tech-item-description {
             font-size: @commonFontSize;
